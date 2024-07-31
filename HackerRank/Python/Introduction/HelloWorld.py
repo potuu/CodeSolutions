769f8a1e924d9674b26a22e36044e29b20339182
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 13:42:49 +0300

    Update HelloWorld.py

--- HackerRank/Python/Introduction/HelloWorld.py
+++ HackerRank/Python/Introduction/HelloWorld.py
@@ -1,1 +1,12 @@
+if __name__ == '__main__':
+    print("Hello, World!")
+
+###  Arithmetic Operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
 
