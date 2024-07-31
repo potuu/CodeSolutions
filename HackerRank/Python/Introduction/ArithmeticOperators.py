6c095e77082a0dcbb7e167bfba69dbef88cc994e
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 13:53:35 +0300

    Create ArithmeticOperators.py

---
+++ HackerRank/Python/Introduction/ArithmeticOperators.py
@@ -0,0 +1,29 @@
+# Input Format
+
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+
+# Constraints
+
+
+
+# Output Format
+
+# Print the three lines as explained above.
+
+# Sample Input 0
+
+# 3
+# 2
+# Sample Output 0
+
+# 5
+# 1
+# 6
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
