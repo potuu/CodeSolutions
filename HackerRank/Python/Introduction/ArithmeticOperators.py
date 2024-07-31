4df4ebee1d039efdfecb60c94df0fdc0fa4144c1
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 13:54:54 +0300

    Update ArithmeticOperators.py

--- HackerRank/Python/Introduction/ArithmeticOperators.py
+++ HackerRank/Python/Introduction/ArithmeticOperators.py
@@ -4,19 +4,17 @@
 # The second line contains the second integer, .
 
 # Constraints
-
-
+#1<=a<=10000000000
+#1<=b<=10000000000
 
 # Output Format
-
 # Print the three lines as explained above.
 
 # Sample Input 0
-
 # 3
 # 2
-# Sample Output 0
 
+# Sample Output 0
 # 5
 # 1
 # 6
