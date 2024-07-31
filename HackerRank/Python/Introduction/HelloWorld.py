58652cf4d66589080477909132ee490c6a5cc80e
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 13:50:41 +0300

    Update HelloWorld.py

--- HackerRank/Python/Introduction/HelloWorld.py
+++ HackerRank/Python/Introduction/HelloWorld.py
@@ -1,3 +1,17 @@
+#Input Format
+
+#You do not need to read any input in this challenge.
+
+#Output Format
+
+#Print Hello, World! to stdout.
+
+#Sample Output 0
+
+#Hello, World!
+
+
+
 if __name__ == '__main__':
     print("Hello, World!")
 
