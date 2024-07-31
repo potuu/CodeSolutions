06d059714e4d760d2286da023c513a7e2c86fa75
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 16:44:45 +0300

    Create TextWrap.py

---
+++ HackerRank/Python/Strings/TextWrap.py
@@ -0,0 +1,47 @@
+import textwrap
+# You are given a string S and width w.
+# Your task is to wrap the string into a paragraph of width w.
+
+# Function Description
+# Complete the wrap function in the editor below.
+
+# wrap has the following parameters:
+# string string: a long string
+# int max_width: the width to wrap to
+
+# Returns
+# string: a single string with newline characters ('\n') where the breaks should be
+
+# Input Format
+# The first line contains a string, .
+# The second line contains the width, .
+
+# Constraints
+# 0<len(string)<1000
+# 0<maxwidth<len(string)
+
+# Sample Input 0
+# ABCDEFGHIJKLIMNOQRSTUVWXYZ
+# 4
+
+# Sample Output 0
+# ABCD
+# EFGH
+# IJKL
+# IMNO
+# QRST
+# UVWX
+# YZ
+
+
+
+def wrap(string, max_width):
+    
+    output=textwrap.fill(string,max_width)
+    
+    return output
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
