9c73db13c84ca9ad3dead3d7082624aa9552ba6c
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 13:42:32 +0300

    Create Division.py

---
+++ HackerRank/Python/Introduction/Division.py
@@ -0,0 +1,16 @@
+
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(round(a/b))
+    print(a/b)
+
+### Loops
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+        i=+i
