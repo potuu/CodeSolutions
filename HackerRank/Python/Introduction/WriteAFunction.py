3066e6a4a2c8f245859f70a80c261faf1c8504b6
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 14:02:27 +0300

    Update WriteAFunction.py

--- HackerRank/Python/Introduction/WriteAFunction.py
+++ HackerRank/Python/Introduction/WriteAFunction.py
@@ -1,5 +1,3 @@
-Task
-
 # Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 
