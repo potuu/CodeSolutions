9c66535c821b07a3d4fda8c5d04eb46c6e09a88c
adil <82321990+potuu@users.noreply.github.com>
2024-07-31 15:59:58 +0300

    Create WhatsYourName.py

---
+++ HackerRank/Python/Strings/WhatsYourName.py
@@ -0,0 +1,31 @@
+# Input Format
+
+# The first line contains the first name, and the second line contains the last name.
+
+# Constraints
+# The length of the first and last names are each ≤ .
+
+# Sample Input 0
+# Ross
+# Taylor
+
+# Sample Output 0
+#Hello Ross Taylor! You just delved into python.
+
+#Explanation 0
+#The input read by the program is stored as a string data type. A string is a collection of characters.
+
+
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello "+first+" "+last+"! You just delved into python.")
+
+if __name__ == '__main__':
