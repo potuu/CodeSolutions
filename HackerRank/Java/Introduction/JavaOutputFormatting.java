a17242694bf21f3667cd78c83665cc4f79456036
Adil Abdullayev <82321990+potuu@users.noreply.github.com>
2024-09-07 23:49:27 +0300

    Create JavaOutputFormatting.java

---
+++ HackerRank/Java/Introduction/JavaOutputFormatting.java
@@ -0,0 +1,46 @@
+// Input Format
+
+// Every line of input will contain a String followed by an integer.
+// Each String will have a maximum of 10 alphabetic characters, and each integer will be in the inclusive range from 0 to 999.
+
+// Output Format
+// In each line of output there should be two columns:
+// The first column contains the String and is left justified using exactly 15 characters.
+// The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+// Sample Input
+// java 100
+// cpp 65
+// python 50
+
+// Sample Output
+// ================================
+// java           100 
+// cpp            065 
+// python         050 
+// ================================
+
+// Explanation
+// Each String is left-justified with trailing whitespace through the first 15 characters. The leading digit of the integer is the 16'th character, and each integer that was less than 3 digits now has leading zeroes.
+
+import java.util.Scanner;
+
+public class Solution
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0; i<3; i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf("%-14s %03d %n", s1, x);
+            // %-14s left-justified from o to 14 for string
+            // %03d padded with leading zero for int
+        }
+         System.out.println("================================");
+    }
+
+}
